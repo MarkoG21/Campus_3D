0c1d1d80650a6b21d8945b0a576e4ac6106543c9
unknown <dbittner@protonmail.com>
2023-01-10 20:37:43 +0100

    added script and position empties

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraController : MonoBehaviour
+{
+    public Button button;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = new Button();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
